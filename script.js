b448652660a6b57a7d134231e23f4a82cd7467af
Jambesh <jambesh@users.noreply.github.com>
2024-04-16 17:41:09 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Example of smooth scrolling for internal links (if you have a single long page)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
